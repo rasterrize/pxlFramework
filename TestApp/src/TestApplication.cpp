7458f0d6a9a08f2d9f8c04c4864e9ba8ba29bb6e
pixel <jyestar343@gmail.com>
2024-02-13 09:45:54 +1100

    Fix TestApp's ImGui code formatting

--- TestApp/src/TestApplication.cpp
+++ TestApp/src/TestApplication.cpp
@@ -279,176 +279,176 @@ namespace TestApp
 
     void TestApplication::OnImGuiRender() // Function only gets called if ImGui is initialized
     {
-            //ImGui::ShowDemoWindow();
+        ImGui::ShowDemoWindow();
 
-            ImGui::SetNextWindowSize(ImVec2(300, 680), ImGuiCond_FirstUseEver);
-            ImGui::SetNextWindowPos(ImVec2(21, 21), ImGuiCond_FirstUseEver);
+        ImGui::SetNextWindowSize(ImVec2(300, 680), ImGuiCond_FirstUseEver);
+        ImGui::SetNextWindowPos(ImVec2(21, 21), ImGuiCond_FirstUseEver);
             
-            ImGui::Begin("pxlFramework: Test App");
+        ImGui::Begin("pxlFramework: Test App");
 
-            ImGui::Text("FPS: %.2f (%.3fms)", pxl::Renderer::GetFPS(), pxl::Renderer::GetFrameTimeMS());
-            ImGui::Text("Clear Colour:");
-            if (ImGui::ColorEdit3("Clear Colour", &m_ClearColour.x))
-                pxl::Renderer::SetClearColour(m_ClearColour);
-            ImGui::Text("Quad Colour:");
-            ImGui::ColorEdit3("Quad Colour", &m_QuadColour.x);
-            if (ImGui::Button("Reload Shader"))
+        ImGui::Text("FPS: %.2f (%.3fms)", pxl::Renderer::GetFPS(), pxl::Renderer::GetFrameTimeMS());
+        ImGui::Text("Clear Colour:");
+        if (ImGui::ColorEdit3("Clear Colour", &m_ClearColour.x))
+            pxl::Renderer::SetClearColour(m_ClearColour);
+        ImGui::Text("Quad Colour:");
+        ImGui::ColorEdit3("Quad Colour", &m_QuadColour.x);
+        if (ImGui::Button("Reload Shader"))
+        {
+            m_Shader->Reload();
+        }
+        ImGui::Text("Camera FOV: %.2f", m_Camera->GetFOV());
+        ImGui::Text("Camera Position: %.3f, %.3f, %.3f", m_CameraPosition.x, m_CameraPosition.y, m_CameraPosition.z);
+        ImGui::Text("Camera Rotation: %.3f, %.3f, %.3f", m_CameraRotation.x, m_CameraRotation.y, m_CameraRotation.z);
+
+        auto stats = pxl::Renderer::GetStats();
+
+        ImGui::Text("Renderer Stats:");
+        ImGui::Text("- Draw Calls: %u", stats.DrawCalls);
+        ImGui::Text("- Vertices: %u", stats.QuadVertexCount + stats.LineVertexCount);
+        ImGui::Text("- Indices: %u", stats.QuadIndexCount);
+        ImGui::Text("- Triangles: %u", stats.GetTriangleCount());
+
+        auto cursorPos = pxl::Input::GetCursorPosition(); 
+
+        ImGui::Text("Cursor Pos: %f, %f", cursorPos.x, cursorPos.y);
+
+        static int i0 = 100;
+        ImGui::InputInt("Blue Quad Amount", &i0);
+        m_BlueQuadAmount = i0;
+
+        static int i1 = 100;
+        ImGui::InputInt("Orange Quad Amount", &i1);
+        m_OrangeQuadAmount = i1;
+
+        ImGui::End();
+
+        // Settings Window
+        ImGui::SetNextWindowSize(ImVec2(330, 200), ImGuiCond_FirstUseEver);
+        ImGui::SetNextWindowPos(ImVec2(929, 21), ImGuiCond_FirstUseEver);
+
+        ImGui::Begin("Settings");
+
+        // Monitor select
+        static int monitorIndex = 1;
+        int step = 1;
+        int monitorCount = pxl::Window::GetMonitorCount();
+        ImGui::SliderInt("Monitor", &monitorIndex, 1, monitorCount);
+        if (monitorIndex <= 0)
+            monitorIndex++; // TODO: turn this into lambda function
+        if (monitorIndex > monitorCount)
+            monitorIndex--;
+
+        // Display mode
+        const char* windowModes[] = { "Windowed", "Borderless", "Fullscreen" };
+        static int windowModeInput = 0;
+        static pxl::WindowMode windowMode;
+        static pxl::WindowMode windowModeToSet;
+
+        if (windowMode != m_Window->GetWindowMode())
+        {
+            switch (m_Window->GetWindowMode())
             {
-                m_Shader->Reload();
-            }
-            ImGui::Text("Camera FOV: %.2f", m_Camera->GetFOV());
-            ImGui::Text("Camera Position: %.3f, %.3f, %.3f", m_CameraPosition.x, m_CameraPosition.y, m_CameraPosition.z);
-            ImGui::Text("Camera Rotation: %.3f, %.3f, %.3f", m_CameraRotation.x, m_CameraRotation.y, m_CameraRotation.z);
-
-            auto stats = pxl::Renderer::GetStats();
-
-            ImGui::Text("Renderer Stats:");
-            ImGui::Text("- Draw Calls: %u", stats.DrawCalls);
-            ImGui::Text("- Vertices: %u", stats.QuadVertexCount + stats.LineVertexCount);
-            ImGui::Text("- Indices: %u", stats.QuadIndexCount);
-            ImGui::Text("- Triangles: %u", stats.GetTriangleCount());
-
-            auto cursorPos = pxl::Input::GetCursorPosition(); 
-
-            ImGui::Text("Cursor Pos: %f, %f", cursorPos.x, cursorPos.y);
-
-            static int i0 = 100;
-            ImGui::InputInt("Blue Quad Amount", &i0);
-            m_BlueQuadAmount = i0;
-
-            static int i1 = 100;
-            ImGui::InputInt("Orange Quad Amount", &i1);
-            m_OrangeQuadAmount = i1;
-
-            ImGui::End();
-
-            // Settings Window
-            ImGui::SetNextWindowSize(ImVec2(330, 200), ImGuiCond_FirstUseEver);
-            ImGui::SetNextWindowPos(ImVec2(929, 21), ImGuiCond_FirstUseEver);
-
-            ImGui::Begin("Settings");
-
-            // Monitor select
-            static int monitorIndex = 1;
-            int step = 1;
-            int monitorCount = pxl::Window::GetMonitorCount();
-            ImGui::SliderInt("Monitor", &monitorIndex, 1, monitorCount);
-            if (monitorIndex <= 0)
-                monitorIndex++; // TODO: turn this into lambda function
-            if (monitorIndex > monitorCount)
-                monitorIndex--;
-
-            // Display mode
-            const char* windowModes[] = { "Windowed", "Borderless", "Fullscreen" };
-            static int windowModeInput = 0;
-            static pxl::WindowMode windowMode;
-            static pxl::WindowMode windowModeToSet;
-
-            if (windowMode != m_Window->GetWindowMode())
-            {
-                switch (m_Window->GetWindowMode())
-                {
-                    case pxl::WindowMode::Windowed:
-                        windowModeInput = 0;
-                        break;
-                    case pxl::WindowMode::Borderless:
-                        windowModeInput = 1;
-                        break;
-                    case pxl::WindowMode::Fullscreen:
-                        windowModeInput = 2;
-                        break;
-                }
-
-                windowMode = m_Window->GetWindowMode();
-            }
-
-            ImGui::Combo("Display Mode", &windowModeInput, windowModes, IM_ARRAYSIZE(windowModes));
-
-            switch (windowModeInput)
-            {
-                case 0:
-                    windowModeToSet = pxl::WindowMode::Windowed;
+                case pxl::WindowMode::Windowed:
+                    windowModeInput = 0;
                     break;
-                case 1:
-                    windowModeToSet = pxl::WindowMode::Borderless;
+                case pxl::WindowMode::Borderless:
+                    windowModeInput = 1;
                     break;
-                case 2:
-                    windowModeToSet = pxl::WindowMode::Fullscreen;
+                case pxl::WindowMode::Fullscreen:
+                    windowModeInput = 2;
                     break;
             }
 
-            // VSync
-            static bool vsync = m_Window->GetGraphicsContext()->GetVSync();
-            ImGui::Checkbox("VSync", &vsync);
+            windowMode = m_Window->GetWindowMode();
+        }
 
-            // Camera FOV
-            static float cameraFOVInput = m_Camera->GetFOV(); // should retrieve cameras FOV
-            static float cameraFOV = m_Camera->GetFOV();
+        ImGui::Combo("Display Mode", &windowModeInput, windowModes, IM_ARRAYSIZE(windowModes));
 
-            ImGui::SliderFloat("Camera FOV", &cameraFOVInput, 30.0f, 120.0f);
+        switch (windowModeInput)
+        {
+            case 0:
+                windowModeToSet = pxl::WindowMode::Windowed;
+                break;
+            case 1:
+                windowModeToSet = pxl::WindowMode::Borderless;
+                break;
+            case 2:
+                windowModeToSet = pxl::WindowMode::Fullscreen;
+                break;
+        }
 
-            // Apply Button
-            if (ImGui::Button("Apply"))
-            {
-                m_Window->SetVSync(vsync);
-                m_Window->SetMonitor(monitorIndex);
-                m_Window->SetWindowMode(windowModeToSet);
+        // VSync
+        static bool vsync = m_Window->GetGraphicsContext()->GetVSync();
+        ImGui::Checkbox("VSync", &vsync);
 
-                cameraFOV = cameraFOVInput;
-            }
+        // Camera FOV
+        static float cameraFOVInput = m_Camera->GetFOV(); // should retrieve cameras FOV
+        static float cameraFOV = m_Camera->GetFOV();
 
-            ImGui::End();
+        ImGui::SliderFloat("Camera FOV", &cameraFOVInput, 30.0f, 120.0f);
 
-            // // Audio player window
-            // ImGui::SetNextWindowSize(ImVec2(330, 400), ImGuiCond_FirstUseEver);
-            // ImGui::SetNextWindowPos(ImVec2(929, 300), ImGuiCond_FirstUseEver);
-            // ImGui::Begin("Audio Player");
+        // Apply Button
+        if (ImGui::Button("Apply"))
+        {
+            m_Window->SetVSync(vsync);
+            m_Window->SetMonitor(monitorIndex);
+            m_Window->SetWindowMode(windowModeToSet);
 
-            // const char* tracks[20]; // should probably set a const max here
+            cameraFOV = cameraFOVInput;
+        }
 
-            // for (int i = 0; i < m_AudioLibrary.size(); i++)
-            // {
-            //     tracks[i] = m_AudioLibrary[i].c_str();
-            // }
+        ImGui::End();
 
-            // static int selectedAudioIndex;
-            // static std::string selectedAudioName = m_AudioLibrary[0];
+        // // Audio player window
+        // ImGui::SetNextWindowSize(ImVec2(330, 400), ImGuiCond_FirstUseEver);
+        // ImGui::SetNextWindowPos(ImVec2(929, 300), ImGuiCond_FirstUseEver);
+        // ImGui::Begin("Audio Player");
 
-            // ImGui::Text("Audio Library");
-            // static int item_current_idx = 0; // Here we store our selection data as an index.
-            // if (ImGui::BeginListBox("##"))
-            // {
-            //     for (int n = 0; n < m_AudioLibrary.size(); n++)
-            //     {
-            //         const bool is_selected = (item_current_idx == n);
-            //         if (ImGui::Selectable(m_AudioLibrary[n].c_str(), is_selected))
-            //         {
-            //             item_current_idx = n;
-            //             selectedAudioName = m_AudioLibrary[n];
-            //         }
+        // const char* tracks[20]; // should probably set a const max here
 
-            //         // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
-            //         if (is_selected)
-            //             ImGui::SetItemDefaultFocus();
-            //     }
-            // ImGui::EndListBox();
-            // }
+        // for (int i = 0; i < m_AudioLibrary.size(); i++)
+        // {
+        //     tracks[i] = m_AudioLibrary[i].c_str();
+        // }
+
+        // static int selectedAudioIndex;
+        // static std::string selectedAudioName = m_AudioLibrary[0];
+
+        // ImGui::Text("Audio Library");
+        // static int item_current_idx = 0; // Here we store our selection data as an index.
+        // if (ImGui::BeginListBox("##"))
+        // {
+        //     for (int n = 0; n < m_AudioLibrary.size(); n++)
+        //     {
+        //         const bool is_selected = (item_current_idx == n);
+        //         if (ImGui::Selectable(m_AudioLibrary[n].c_str(), is_selected))
+        //         {
+        //             item_current_idx = n;
+        //             selectedAudioName = m_AudioLibrary[n];
+        //         }
+
+        //         // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+        //         if (is_selected)
+        //             ImGui::SetItemDefaultFocus();
+        //     }
+        // ImGui::EndListBox();
+        // }
             
-            // if (ImGui::Button("Play"))
-            // {
-            //     pxl::AudioManager::Play(selectedAudioName);
-            // }
+        // if (ImGui::Button("Play"))
+        // {
+        //     pxl::AudioManager::Play(selectedAudioName);
+        // }
 
-            // if (ImGui::Button("Pause"))
-            // {
-            //     pxl::AudioManager::Pause(selectedAudioName);
-            // }
+        // if (ImGui::Button("Pause"))
+        // {
+        //     pxl::AudioManager::Pause(selectedAudioName);
+        // }
 
-            // if (ImGui::Button("Stop"))
-            // {
-            //     pxl::AudioManager::Stop(selectedAudioName);
-            // }
+        // if (ImGui::Button("Stop"))
+        // {
+        //     pxl::AudioManager::Stop(selectedAudioName);
+        // }
 
-            //ImGui::End();
+        //ImGui::End();
     }
 }
