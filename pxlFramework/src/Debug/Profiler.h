5873746dbe8cd7c9703117c433e13f0e7a7f775a
rasterrize <jyestar343@gmail.com>
2024-08-07 10:13:06 +1000

    Add named scope profiler functions

--- pxlFramework/src/Debug/Profiler.h
+++ pxlFramework/src/Debug/Profiler.h
@@ -4,8 +4,10 @@
     #include <tracy/Tracy.hpp>
 
     #define PXL_PROFILE_SCOPE ZoneScoped;
+    #define PXL_PROFILE_SCOPE_NAMED(zoneName) ZoneScopedN(zoneName);
     #define PXL_PROFILE_FRAME_END FrameMark;
 #else
     #define PXL_PROFILE_SCOPE
+    #define PXL_PROFILE_SCOPE_NAMED(zoneName)
     #define PXL_PROFILE_FRAME_END
 #endif
