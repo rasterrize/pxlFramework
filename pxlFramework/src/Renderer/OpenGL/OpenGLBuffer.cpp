61230abb7ec0d8ac0d90b57f0a3c9f72daf3a316
rasterrize <jyestar343@gmail.com>
2024-10-01 06:59:41 +1000

    Fix undefined behaviour for OpenGL
    This change is so important because it could be the fix for the long running random renderer artifacts bug. Only just come across it now since my indices were consistently incorrect for zero reason.

--- pxlFramework/src/Renderer/OpenGL/OpenGLBuffer.cpp
+++ pxlFramework/src/Renderer/OpenGL/OpenGLBuffer.cpp
@@ -5,9 +5,12 @@ namespace pxl
     OpenGLBuffer::OpenGLBuffer(GPUBufferUsage usage, GPUBufferDrawHint drawHint, uint32_t size, const void* data)
         : m_Usage(ToGLUsageEnum(usage)), m_DrawHint(ToGLDrawHint(drawHint))
     {
-        glCreateBuffers(1, &m_RendererID);             // "CreateBuffers" instead of "GenBuffers" initializes the object on creation rather than on binding. However, The buffer still needs to be bound to the current OGL context
-        glBindBuffer(m_Usage, m_RendererID);           // Initializes the buffer object (not the data) to the context
-        glBufferData(m_Usage, size, data, m_DrawHint); // Initialize the data storage and copy the data into the memory
+        glCreateBuffers(1, &m_RendererID); // "CreateBuffers" instead of "GenBuffers" initializes the object on creation rather than on binding. However, The buffer still needs to be bound to the current OGL context
+
+        // GL_ELEMENT_ARRAY_BUFFER is not valid without an actively bound VAO (possible since core profile doesn't have a default VAO)
+        // Binding with GL_ARRAY_BUFFER allows the data to be loaded regardless of VAO state.
+        glBindBuffer(GL_ARRAY_BUFFER, m_RendererID);           // Initializes the buffer object (not the data) to the context
+        glBufferData(GL_ARRAY_BUFFER, size, data, m_DrawHint); // Initialize the data storage and copy the data into the memory
     }
 
     OpenGLBuffer::~OpenGLBuffer()
