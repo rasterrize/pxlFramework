0ac966fa876b551eaaf9c719a599401c89077223
Jye Curtis-Smith <jyestar343@gmail.com>
2026-02-26 21:23:19 +1100

    Move Cursor class to separate file

---
+++ pxlFramework/src/Core/Cursor.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+#include "Image.h"
+
+namespace pxl
+{
+    enum class CursorMode
+    {
+        Normal,
+        Hidden,
+        Disabled,
+        Captured,
+    };
+
+    enum class StandardCursor
+    {
+        Arrow,
+        IBeam,
+        Crosshair,
+        Hand,
+        HResize,
+        VResize,
+    };
+
+    class Cursor
+    {
+    public:
+        Cursor(const std::shared_ptr<Image>& image, int32_t hotspotX = 0, int32_t hotspotY = 0)
+        {
+            GLFWimage glfwImage;
+            glfwImage.width = image->Metadata.Size.Width;
+            glfwImage.height = image->Metadata.Size.Height;
+            glfwImage.pixels = image->Buffer.data();
+
+            m_Cursor = glfwCreateCursor(&glfwImage, hotspotX, hotspotY);
+        }
+
+        GLFWcursor* GetNativeCursor() const { return m_Cursor; }
+
+    private:
+        GLFWcursor* m_Cursor = nullptr;
+    };
+}
--- pxlFramework/src/Core/Input.h
+++ pxlFramework/src/Core/Input.h
@@ -4,6 +4,7 @@
 
 #include <glm/vec2.hpp>
 
+#include "Cursor.h"
 #include "Gamepad.h"
 #include "KeyCodes.h"
 #include "MouseCodes.h"
@@ -11,43 +12,6 @@
 
 namespace pxl
 {
-    enum class CursorMode
-    {
-        Normal,
-        Hidden,
-        Disabled,
-        Captured,
-    };
-
-    enum class StandardCursor
-    {
-        Arrow,
-        IBeam,
-        Crosshair,
-        Hand,
-        HResize,
-        VResize,
-    };
-
-    class Cursor
-    {
-    public:
-        Cursor(const std::shared_ptr<Image>& image, int32_t hotspotX = 0, int32_t hotspotY = 0)
-        {
-            GLFWimage glfwImage;
-            glfwImage.width = image->Metadata.Size.Width;
-            glfwImage.height = image->Metadata.Size.Height;
-            glfwImage.pixels = image->Buffer.data();
-
-            m_Cursor = glfwCreateCursor(&glfwImage, hotspotX, hotspotY);
-        }
-
-        GLFWcursor* GetNativeCursor() { return m_Cursor; }
-
-    private:
-        GLFWcursor* m_Cursor = nullptr;
-    };
-
     class Input
     {
     public:
