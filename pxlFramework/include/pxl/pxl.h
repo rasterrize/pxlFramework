9e3656917a84de38ac9a072f4c67d4724f70a54b
Jye Curtis-Smith <jyestar343@gmail.com>
2026-03-25 18:01:38 +1100

    Reorganise pxl.h

--- pxlFramework/include/pxl/pxl.h
+++ pxlFramework/include/pxl/pxl.h
@@ -6,51 +6,65 @@
 #include "../src/Core/Assert.h"
 #include "../src/Core/Colour.h"
 #include "../src/Core/Config.h"
+#include "../src/Core/Cursor.h"
 #include "../src/Core/Image.h"
 #include "../src/Core/Input.h"
+#include "../src/Core/InputSystem.h"
 #include "../src/Core/KeyCodes.h"
 #include "../src/Core/Logging/ApplicationLog.h"
 #include "../src/Core/MouseCodes.h"
 #include "../src/Core/Platform.h"
+#include "../src/Core/Size.h"
 #include "../src/Core/Stopwatch.h"
 #include "../src/Core/Window.h"
 
 // Events
 #include "../src/Core/Events/Event.h"
 #include "../src/Core/Events/EventHandler.h"
+#include "../src/Core/Events/GamepadEvents.h"
 #include "../src/Core/Events/KeyboardEvents.h"
 #include "../src/Core/Events/MouseEvents.h"
 #include "../src/Core/Events/WindowEvents.h"
-#include "../src/Core/Events/GamepadEvents.h"
 
 // Renderer
 #include "../src/Renderer/BufferLayout.h"
 #include "../src/Renderer/Camera.h"
+#include "../src/Renderer/GPUBuffer.h"
+#include "../src/Renderer/GPUResource.h"
+#include "../src/Renderer/GraphicsAPI.h"
+#include "../src/Renderer/GraphicsAPIType.h"
 #include "../src/Renderer/GraphicsContext.h"
+#include "../src/Renderer/GraphicsPipeline.h"
+#include "../src/Renderer/Mesh.h"
 #include "../src/Renderer/OrthographicCamera.h"
 #include "../src/Renderer/PerspectiveCamera.h"
-#include "../src/Renderer/GraphicsPipeline.h"
 #include "../src/Renderer/Primitives/Quad.h"
 #include "../src/Renderer/Renderer.h"
+#include "../src/Renderer/RendererConfig.h"
 #include "../src/Renderer/Shader.h"
+#include "../src/Renderer/ShaderCompiler.h"
+#include "../src/Renderer/ShaderManager.h"
 #include "../src/Renderer/Texture.h"
+#include "../src/Renderer/TextureHandler.h"
 #include "../src/Renderer/UniformLayout.h"
+#include "../src/Renderer/VertexBatch.h"
 #include "../src/Renderer/Vertices.h"
 
-// Debug
-#include <imgui.h> // so applications can use ImGui functions
-
-// NOTE: For now we provide glm to the user, since it's the only math library we use.
-// In the future this could be a custom math library.
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include "../src/Debug/Profiler.h"
-
 // Utils
+#include "../src/Utils/Easing.h"
 #include "../src/Utils/FileSystem.h"
 #include "../src/Utils/Random.h"
+
+// Debug
+#include "../src/Debug/Profiler.h"
 
 #ifdef PXL_ENABLE_MODULE_DISCORD
     #include "../modules/discord/src/DiscordRPC.h"
 #endif
+
+// Allows applications to use ImGui functions
+#include <imgui.h>
+
+// NOTE: For now we provide glm to the user, since it's the only math library we use.
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
